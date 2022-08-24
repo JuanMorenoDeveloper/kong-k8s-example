51aff424aeb97a3ee52d10654d9843ca21c37b49
Juan Moreno <juan.moreno@project44.com>
2022-08-24 16:11:01 -0300

    Update permissions handling.

--- custom-implementation/py-plugins/header_check.py
+++ custom-implementation/py-plugins/header_check.py
@@ -11,6 +11,13 @@ priority = 1000
 
 
 class Plugin:
+    # Load values from artifact
+    permissions_mapping = {
+      "PERMISSION_1": "1",
+      "PERMISSION_2": "2",
+      "PERMISSION_3": "3"
+    }
+
     def __init__(self, config):
         self.config = config
         self.storage = token_bucket.MemoryStorage()
@@ -27,13 +34,20 @@ class Plugin:
         token_count = self.storage.get_token_count('key')
         if header_value is None:
             kong.response.set_header("token_count", str(token_count))
+            kong.service.request.set_header("token_count", str(token_count))
             return kong.response.error(400, f"Missing header: {header_name}, "
                                             f"token_count: {token_count}")
         else:
             kong.service.request.set_header("now", str(datetime.utcnow()))
             kong.service.request.set_header("token_count", str(token_count))
+            kong.service.request.clear_header(header_name)
+            kong.service.request.set_header("x-permissions", self.encode_permissions(header_value))
         if token_count < 1:
             kong.response.set_header("token_count", str(token_count))
-            return kong.response.error(429, f"Reached rate limit, num_tokens must be >= 1, "
+            return kong.response.error(429, "Reached rate limit, "
+                                            "num_tokens must be >= 1, "
                                             f"token_count: {token_count}")
 
+    def encode_permissions(self, permission_header):
+        # Custom logic
+        return self.permissions_mapping[permission_header]
